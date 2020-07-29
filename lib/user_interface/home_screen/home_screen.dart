7f1651bc89f1b9db864a975566ab2613839d0f1c
Samip <samipsagar@gmail.com>
2020-07-29 23:42:25 +1000

    creates quiz screen using bloc pattern with mock data

--- lib/user_interface/home_screen/home_screen.dart
+++ lib/user_interface/home_screen/home_screen.dart
@@ -1,44 +1,56 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_quiz/shared/widgets/custom_button.dart';
-import 'package:flutter_quiz/utilities/base_screen_routing.dart';
+import 'package:flutter_quiz/user_interface/widgets/rounded_bordered_button.dart';
+import 'package:flutter_quiz/user_interface/quiz_screen/quiz_screen.dart';
+import 'package:flutter_quiz/utilities/base_screen.dart';
 
 class HomeScreen extends BaseStatelessWidget {
+  static const String routeName = '/';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _getBody(),
+      body: _getBody(context),
     );
   }
 
-  Widget _getBody() {
-    var titleWidget = Text(
-      "Calculate Points",
-      style: TextStyle(
-        color: Colors.amber,
-        fontWeight: FontWeight.bold,
-        fontSize: 24,
-      ),
-    );
-
-    var subtitleWidget = Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      child: Text(
-          "By answering this 3 mins survey you will know how much points you have aquired.."),
-    );
-
+  Widget _getBody(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(32.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Spacer(),
-          titleWidget,
-          subtitleWidget,
+          _getTitleWidget("Calculate Points"),
+          _getSubtitleWidget(
+              "By answering this 3 mins survey you will know how much points you have aquired.."),
           Spacer(),
-          RoundedRectangleButton(title: "Continue", onPress: () => {}),
+          RoundedBorderedButton(
+            title: "Continue",
+            onPress: () =>
+                Navigator.of(context).pushNamed(QuizScreen.routeName),
+            isSelected: true,
+          ),
         ],
       ),
+    );
+  }
+
+  Widget _getTitleWidget(String title) {
+    return Text(
+      title,
+      style: TextStyle(
+        color: Colors.amber,
+        fontWeight: FontWeight.bold,
+        fontSize: 24,
+      ),
+    );
+  }
+
+  Widget _getSubtitleWidget(String subtitle) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: Text(subtitle),
     );
   }
 }
--- lib/utilities/router.dart
+++ lib/utilities/router.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+import '../user_interface/quiz_screen/quiz_screen.dart';
 import '../user_interface/home_screen/home_screen.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case '/':
       return MaterialPageRoute(builder: (_) => HomeScreen());
+
+    case QuizScreen.routeName:
+      return MaterialPageRoute(builder: (_) => QuizScreen());
 
     default:
       return MaterialPageRoute(
